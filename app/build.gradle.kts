dee64857c38e743faefa1f62f2d9c5511db9cae9
aniruddhadabade <aniruddhadabade123@gmail.com>
2025-01-18 02:04:50 +0530

    mpfile chart added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,8 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
+    implementation ("com.android.volley:volley:1.2.1")
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
